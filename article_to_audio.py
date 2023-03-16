cf821a6159a55c8fed030f201f071f7defb13470
Zach Williams <zachwilliams@Zachs-MBP.attlocal.net>
2023-03-16 15:10:18 -0700

    initialize repo with pipsqueak v1

---
+++ article_to_audio.py
@@ -0,0 +1,38 @@
+import sys
+import requests
+from newspaper import Article
+from gtts import gTTS
+from pydub import AudioSegment
+
+def extract_text_from_url(url):
+    try:
+        article = Article(url)
+        article.download()
+        article.parse()
+        return article.text
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+def text_to_audio(text, output_filename):
+    try:
+        tts = gTTS(text, lang="en")
+        tts.save(f"{output_filename}.mp3")
+        print(f"Audio saved as {output_filename}.mp3")
+    except Exception as e:
+        print("Error:", e)
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python article_to_audio.py <url> [<output_filename>]")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    output_filename = sys.argv[2] if len(sys.argv) > 2 else "article_audio"
+
+    text = extract_text_from_url(url)
+    if text:
+        text_to_audio(text, output_filename)
+
+if __name__ == "__main__":
+    main()
